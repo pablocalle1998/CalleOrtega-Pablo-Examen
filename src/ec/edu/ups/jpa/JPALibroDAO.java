4373945148ae5c1a5eda5943069f2c17026ffc81
pablocalle1998 <prosebascalle@hotmail.com>
2020-06-02 12:14:14 -0500

    libro dao

--- src/ec/edu/ups/jpa/JPALibroDAO.java
+++ src/ec/edu/ups/jpa/JPALibroDAO.java
@@ -1,5 +1,16 @@
 package ec.edu.ups.jpa;
 
-public class JPALibroDAO {
+import javax.persistence.Persistence;
+
+import ec.edu.ups.dao.LibroDAO;
+import ec.edu.ups.entidad.Libro;
+
+public class JPALibroDAO extends JPAGenericDAO<Libro, String> implements LibroDAO{
+
+	public JPALibroDAO() {
+		super(Libro.class);
+		this.em = Persistence.createEntityManagerFactory("CalleOrtega-Pablo-Examen").createEntityManager();
+		// TODO Auto-generated constructor stub
+	}
 
 }
